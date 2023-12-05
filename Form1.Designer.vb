6a9a19610e0e348abebaba683d64a35536ff13e7
Genaro123-rgb <cardenasgenaro81@gmail.com>
2023-12-04 23:27:45 -0600

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        ProdSeleccionado = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(192, 18)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(551, 65)
+        Label1.TabIndex = 0
+        Label1.Text = "Producto seleccionado:"
+        ' 
+        ' ProdSeleccionado
+        ' 
+        ProdSeleccionado.AutoSize = True
+        ProdSeleccionado.Font = New Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        ProdSeleccionado.Location = New Point(394, 199)
+        ProdSeleccionado.Name = "ProdSeleccionado"
+        ProdSeleccionado.Size = New Size(66, 65)
+        ProdSeleccionado.TabIndex = 1
+        ProdSeleccionado.Text = "--"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(879, 502)
+        Controls.Add(ProdSeleccionado)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ProdSeleccionado As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.IO.Ports
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Dim WithEvents arduinoPort As New SerialPort("COM6", 9600) ' Cambia "COM" al puerto correcto
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        arduinoPort.Open()
+    End Sub
+
+    Private Sub arduinoPort_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles arduinoPort.DataReceived
+        Dim data As String = arduinoPort.ReadLine()
+        Me.Invoke(Sub() HandleProductSelection(data))
+    End Sub
+
+    Private Sub HandleProductSelection(data As String)
+        ' Mostrar la selección de productos en un TextBox o Label
+        ProdSeleccionado.Text = data
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If arduinoPort.IsOpen Then
+            arduinoPort.Close()
+        End If
+    End Sub
+End Class
+
+
+
+
